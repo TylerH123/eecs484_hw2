14be97e4fdf6644086a4642a3da8b3246b132de2
Tyler <tylhuang@umich.edu>
2022-09-25 22:09:31 -0400

    push

--- loadData.sql
+++ loadData.sql
@@ -1,34 +1,102 @@
 INSERT INTO Students 
-(Name, Major)
-VALUES 
-('TYLER', 'CS'),
-('JEFF', 'CS'),
-('AMY', 'ART'),
-('BOB', 'MATH'),
-('CAM', 'EE'),
-('DAVID', 'EE'),
-('EDWARD', 'EE'),
-('FRED', 'EE'),
-('GEORGE', 'EE'),
-('HARRY', 'EE'),
-('IZZY', 'EE'),
+(Name, Major) VALUES 
+('TYLER', 'CS');
+
+INSERT INTO Students 
+(Name, Major) VALUES
+('JEFF', 'CS');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('AMY', 'ART');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('BOB', 'MATH');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('CAM', 'EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('DAVID', 'EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('EDWARD', 'EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('FRED', 'EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('GEORGE','EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('HARRY', 'EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
+('IZZY', 'EE');
+
+INSERT INTO Students 
+(Name, Major) VALUES 
 ('JARED', 'EE');
 
-INSERT INTO Courses VALUES 
-(0, 'EECS 183'),
+INSERT INTO Courses 
+VALUES 
+(0, 'EECS 183');
+
+INSERT INTO Courses 
+VALUES 
 (1, 'EECS 484');
 
-INSERT INTO Enrollments VALUES 
-(0, 12),
-(0, 11),
-(0, 10),
-(0, 9),
-(0, 8),
-(0, 7), 
-(0, 6),
-(0, 5),
-(0, 4), 
-(0, 3), 
-(0, 2),
-(0, 1), 
-(1, 1);
+INSERT INTO Courses 
+VALUES 
+(2, 'HISTART 292');
+
+INSERT INTO Courses 
+VALUES 
+(3, 'COMM 306');
+
+INSERT INTO Enrollments 
+VALUES (0, 12); 
+
+INSERT INTO Enrollments 
+VALUES (0, 11),
+
+INSERT INTO Enrollments 
+VALUES (0, 10),
+
+INSERT INTO Enrollments 
+VALUES (0, 9);
+
+INSERT INTO Enrollments 
+VALUES (0, 8);
+
+INSERT INTO Enrollments 
+VALUES (0, 7);
+
+INSERT INTO Enrollments 
+VALUES (0, 6);
+
+INSERT INTO Enrollments 
+VALUES (0, 5);
+
+INSERT INTO Enrollments 
+VALUES (0, 4); 
+
+INSERT INTO Enrollments 
+VALUES (0, 3); 
+
+INSERT INTO Enrollments 
+VALUES (0, 2);
+
+INSERT INTO Enrollments 
+VALUES (0, 1); 
+
+INSERT INTO Enrollments 
+VALUES (1, 1);

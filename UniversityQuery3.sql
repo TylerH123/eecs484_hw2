5d279f72e4649ed39fb8f210d9909a8494c90610
Tyler <tylhuang@umich.edu>
2022-09-26 15:30:47 -0400

    push

--- UniversityQuery3.sql
+++ UniversityQuery3.sql
@@ -1,29 +1,3 @@
--- SELECT DISTINCT S.SID
--- FROM Students S, Courses C1, Courses C2, Courses C3, Enrollments E1, Enrollments E2, Enrollments E3
--- WHERE (S.SID = E1.SID AND S.SID = E2.SID AND S.SID = E3.SID AND E1.CID = C1.CID AND E2.CID = C2.CID AND E3.CID = C3.CID AND
---         (C1.C_Name = 'EECS442' AND C2.C_NAME = 'EECS445' AND C3.C_NAME = 'EECS492')) OR 
---       (S.SID = E1.SID AND S.SID = E2.SID AND E1.CID = C1.CID AND E2.CID = C2.CID AND
---         (C1.C_Name = 'EECS482' AND C2.C_NAME = 'EECS486')) OR
---       (S.SID = E1.SID AND E1.CID = C1.CID AND C1.C_Name = 'EECS281')
--- ORDER BY S.SID; 
--- SELECT SID FROM (
---   SELECT S.SID
---   FROM Students S, Courses C, Enrollments E
---   WHERE S.SID = E.SID AND E.CID = C.CID AND C.C_Name = 'EECS281' 
---   UNION 
---   SELECT S.SID 
---   FROM Students S2, Courses C1, Courses C2, Enrollments E1, Enrollments E2
---   WHERE S.SID = E1.SID AND S.SID = E2.SID AND E1.CID = C1.CID AND E2.CID = C2.CID AND (C1.C_Name = 'EECS482' AND C2.C_Name = 'EECS486') 
---   UNION 
---   SELECT S3.SID 
---   FROM Students S, Courses C1, Courses C2, Courses C3, Enrollments E1, Enrollments E2, Enrollments E3
---   WHERE S.SID = E1.SID AND S.SID = E2.SID AND S.SID = E3.SID AND E1.CID = C1.CID AND E2.CID = C2.CID AND E3.CID = C3.CID AND
---         (C1.C_Name = 'EECS442' AND C2.C_NAME = 'EECS445' AND C3.C_NAME = 'EECS492')
--- )
--- ORDER BY S.SID; 
-
-
-
 SELECT SID 
 FROM (
   SELECT S.SID
@@ -39,4 +13,5 @@ FROM (
   SELECT S.SID
   FROM Students S, Courses C1, Enrollments E1
   WHERE S.SID = E1.SID AND E1.CID = C1.CID AND C1.C_Name = 'EECS281'
-) ORDER BY SID;
+) 
+ORDER BY SID;
